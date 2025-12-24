b0cfc8c60463d4ac49dcca832cc87db3a350e6a7
superheroes6 <santibox888@gmail.com>
2025-12-24 14:10:53 +0100

    commit final

--- src/Calculadora/CalCONSOLA.java
+++ src/Calculadora/CalCONSOLA.java
@@ -4,20 +4,33 @@ import java.util.Scanner;
 
 public class CalCONSOLA {
     public static void main(String[] args) {
-		Scanner num1 = new Scanner(System.in);
-		System.out.print("Dame el primer numero para realizar la operacion: ");
-		int numero1 = num1.nextInt();
-		num1.close();
-		
-		Scanner operador = new Scanner(System.in);
-		System.out.print("Dame el primer numero para realizar la operacion: ");
-		String operacion = operador.nextLine();
-		operador.close();
-		
-		Scanner num2 = new Scanner(System.in);
-		System.out.print("Dame el segundo numero para realizar la operacion: ");
-		int numero2 = num1.nextInt();
-		num2.close();
-		
+		Scanner operacion = new Scanner(System.in);
+		boolean condicional = true;
+		while (condicional == true){
+			System.out.print("Escribe la operacion que quieras hacer (debe contener espacios) o stop para parar: ");
+			String entrada = operacion.nextLine();
+			if (entrada.equals("stop")){
+				condicional = false;
+				System.out.println("Apagando calculadora.");
+			}else{
+				String[] partes = entrada.split(" ");
+				if (partes.length != 3){
+					System.out.println("Error, la operacion debe tener dos numeros y una operacion por lo menos");
+					break;
+				}else{
+					int numero1 = Integer.parseInt(partes[0]);
+					int numero2 = Integer.parseInt(partes[2]);
+					if (partes[1].equals("+")){
+					System.out.println("Resultado = " + (numero1 + numero2));
+					}else if (partes[1].equals("-")){
+					System.out.println("Resultado = " + (numero1 - numero2));
+					}else if (partes[1].equals("*")){
+					System.out.println("Resultado = " + (numero1 * numero2));
+					}else if (partes[1].equals("/")){
+					System.out.println("Resultado = " + (numero1 / numero2));
+					}
+				}
+			}
+		}
 	}
 }
